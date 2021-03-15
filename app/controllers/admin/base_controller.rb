1bb738a067fa61fa432eb8db173cdcee47a9c2ff
Yuan Li <202070317@163.com>
2021-03-15 16:23:58 +0800

    配置文件，主控制器

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -1,6 +1,5 @@
 class Admin::BaseController < ApplicationController
   include SessionsHelper
-  before_action :store_location
   before_action :authenticate_admin!
   layout :set_layout
   authorize_resource :class => false
@@ -22,15 +21,6 @@ class Admin::BaseController < ApplicationController
     if !sign_in?
       redirect_to admin_sign_in_path
     end if request.path =~ /\/admin/
-  end
-
-  def store_location
-    session[:return_to] = request.fullpath if request.get? and request.format.html? and request.fullpath !~ /sign_.*|sessions|registrations|passwords|auth/
-  end
-
-  def redirect_back_or(default)
-    redirect_to(session[:return_to] || default)
-    session.delete(:return_to)
   end
 
   def set_layout
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::Base
+  before_action :store_location
+
+  private
+
+  def store_location
+    session[:return_to] = request.fullpath if request.get? and request.format.html? and request.fullpath !~ /sign_.*|sessions|registrations|passwords|auth/
+  end
+
+  def redirect_back_or(default)
+    redirect_to(session[:return_to] || default)
+    session.delete(:return_to)
+  end
 end
